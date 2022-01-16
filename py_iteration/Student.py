e232884b5430d1b135a86e26e6bddd31735d6e28
sburakozcan <84351922+sburakozcan@users.noreply.github.com>
2022-01-16 15:13:34 +0300

    Student.py CALCULATIONS ADDED
    Completed credit and gpa calculations are done.

--- py_iteration/Student.py
+++ py_iteration/Student.py
@@ -11,6 +11,10 @@ class Student:
         self.requested_courses = []
         self.schedule = None  # it keeps student's weekly course program. Schedule class.
         self.advisor = None
+        self.success_coefficient = 0.0
+        self.cumulative_credit = 0
+        self.cumulative_success_coefficient = 0.0
+        self.completed_credit = 0
 
     def set_grade(self):
         grade = 21 - int(self.student_id[4:6])
@@ -43,13 +47,47 @@ class Student:
 
         return failed_courses
 
-    # WILL BE ADDED.
+
     def get_completed_credit(self):
-        return 160
+        for key in self.taken_courses:
+            if self.taken_courses[key] == 'FF' or self.taken_courses[key] == 'FD':
+                continue
+            self.completed_credit += int(str(key.credit)[0])
+        return self.completed_credit
 
     def send_courses_to_advisor(self):
         self.advisor.check_courses(self.schedule, self.requested_courses, self.get_completed_credit())
+        
 
     def calculate_gpa(self):
-        self.gpa = 2.4
+
+        if not self.taken_courses:
+            return 0.00
+        
+        for key in self.taken_courses:
+            if self.taken_courses[key] == 'FF':
+                self.success_coefficient = 0.0
+            if self.taken_courses[key] == 'FD':
+                self.success_coefficient = 0.5
+            if self.taken_courses[key] == 'DD':
+                self.success_coefficient = 1.0
+            if self.taken_courses[key] == 'DC':
+                self.success_coefficient = 1.5
+            if self.taken_courses[key] == 'CC':
+                self.success_coefficient = 2.0
+            if self.taken_courses[key] == 'CB':
+                self.success_coefficient = 2.5
+            if self.taken_courses[key] == 'BB':
+                self.success_coefficient = 3.0
+            if self.taken_courses[key] == 'BA':
+                self.success_coefficient = 3.5
+            if self.taken_courses[key] == 'AA':
+                self.success_coefficient = 4.0
+        
+            key.credit = int(str(key.credit)[0])
+            self.cumulative_credit += key.credit
+            self.cumulative_success_coefficient += key.credit * self.success_coefficient
+
+        self.gpa = (self.cumulative_success_coefficient /  self.cumulative_credit)
+        self.gpa = "%.2f" % self.gpa
         return self.gpa
