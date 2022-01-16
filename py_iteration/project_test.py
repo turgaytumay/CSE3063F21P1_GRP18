0550c6a55de03e9d858df9f6c239fb6ba8de6237
huseynhasilci <huseynhasilci@gmail.com>
2022-01-16 15:32:27 +0300

    Trying to add test to the system. But it's not finished due to the time issues.

---
+++ py_iteration/project_test.py
@@ -0,0 +1,77 @@
+import unittest
+from Advisor import *
+from Course import *
+from CourseManager import *
+from CourseSection import *
+from FileManager import *
+from FteCourse import *
+from main import *
+from NteCourse import *
+from RegisterationManager import *
+from Schedule import *
+from ScheduleManager import *
+from Student import *
+from StudentManager import *
+from TeCourse import *
+
+class TestAdvisor(unittest.TestCase):
+
+    def test_check_courses(self):
+        pass
+
+    def test_check_collision(self):
+        pass
+
+    def test_check_requirement(self):
+        pass
+
+    def test_approval_course(self):
+        pass
+
+class TestCourse(unittest.TestCase):
+
+    def check_course_requirement(self):
+        pass
+
+class TestCourseManager(unittest.TestCase):
+
+    def test_create_courses(self):
+        pass
+
+    def test_get_past_semester_courses(self):
+        pass
+
+    def test_get_current_semester_courses(self):
+        pass
+
+    def test_get_five_elective_courses(self):
+        pass
+
+    def test_print_schedule(self):
+        pass
+
+
+class TestCourseSection(unittest.TestCase):
+    def test_add_to_schedule(self):
+        pass
+
+
+class TestFileManager(unittest.TestCase):
+    def test_read_courses(self):
+        pass
+
+
+class Testmain(unittest.TestCase):
+    def test_start_app(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
